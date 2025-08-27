50d78315e625927d25cfb76139f15344bf0e3277
Jon Yurek <jyurek@gmail.com>
2025-08-27 09:29:49 -0400

    lol rainbow prompt

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -18,14 +18,6 @@ function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
 
-function parse_dot_dirty {
-  $(dot diff-index --quiet HEAD 2>/dev/null) && echo -n '34m' || echo -n '35m'
-}
-
-function parse_dot_branch {
-  dot branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
-}
-
 function latest_command {
   history | tail -n 1 | sed 's/[0-9 ]*\(.*\)/\1/'
 }
@@ -36,33 +28,32 @@ function color_text {
 
 function prompt_command_function
 {
-  last_result=$?
+  last_result=$(color_text $? "64;64;64")
   timer_stop
+  color_runtime=$(color_text ${timer_show}s "192;192;64")
 
-  last_result="\[\e[33m\]$last_result\[\e[0m\]"
-  titlebar_last_command="\[\e]2;$(latest_command)\a\]"
+  cwd=$(pwd)
+  cwd=$(echo $cwd | sed 's/\/Users\/jyurek\/Development\/clients/\+/')
+  cwd=$(echo $cwd | sed 's/~\/Development\/clients/\+/')
+  color_cwd=$(color_text $cwd "64;192;64")
+
+  git_dirty=$(parse_git_dirty)
+  git_dirty=${git_dirty:+" \[\e[31m\]$git_dirty\[\e[0m\]"}
 
   git_branch=$(parse_git_branch)
-  git_dirty=$(parse_git_dirty)
+  git_branch=${git_branch:+"(\[\e[35m\]${git_branch}\[\e[0m\]${git_dirty})"}
 
-  git_dirty=${git_dirty:+" \[\e[31m\]$git_dirty\[\e[0m\]"}
-  git_branch=${git_branch:+" (\[\e[35m\]${git_branch}\[\e[0m\]${git_dirty})"}
-  dot_dirty=$(parse_dot_dirty)
-  dot_branch=$(parse_dot_branch)
-
-  current_node=
-  type nvm > /dev/null 2>&1 && current_node=$(nvm current)
+  current_node=type nvm > /dev/null 2>&1 && current_node=$(nvm current)
   color_node=$(color_text $current_node "231;231;64")
 
-  current_ruby=
-  type chruby > /dev/null 2>&1 && current_ruby=$(chruby | ag \\\* | cut -d" " -f 3)
+  current_ruby=type chruby > /dev/null 2>&1 && current_ruby=$(chruby | ag \\\* | cut -d" " -f 3)
   # current_ruby=$(cat ~/.tool-versions | ag ruby | cut -d" " -f 2)
   color_ruby=$(color_text $current_ruby "231;64;64")
 
   current_elixir=$(asdf list elixir | ag \\* | cut -d* -f2)
   color_elixir=$(color_text $current_elixir  "255;192;128")
 
-  PS1="$last_result \[\e[32m\]${timer_show}s\[\e[0m\] $color_ruby $color_elixir $color_node \[\e[32m\]\w\[\e[0m\]$git_branch \$ "
+  PS1="$last_result $color_runtime $color_ruby $color_elixir $color_node $color_cwd $git_branch \$ "
 }
 
 export PROMPT_COMMAND=prompt_command_function
